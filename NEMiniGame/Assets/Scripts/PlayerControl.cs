dcbd1347bc90d0983b0f9d8c5a8e4744cad6b14d
mxrhyx233 <mxr233456@sina.com>
2019-05-12 23:01:50 +0800

    基本控制
    移动和指示线

---
+++ NEMiniGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Camera _camera;
+    public Rigidbody rig;
+    public Vector3 dir;
+    public float speed, rspeed;
+    public bool show_line;
+    private float tspeed, trspeed;//前进速度，旋转速度
+    private Vector3 mouse1, mouse2;//第一次鼠标，第二次鼠标位置
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+        rig = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouse1 = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            mouse2 = Input.mousePosition;
+            if(Vector3.Distance(mouse1,mouse2)>10)
+            {
+                show_line = true;
+                Vector3 screenP = _camera.WorldToScreenPoint(rig.position);
+                mouse1.z = screenP.z;
+                mouse2.z = screenP.z;
+                Vector3 dir1 = _camera.ScreenToWorldPoint(mouse1);
+                Vector3 dir2 = _camera.ScreenToWorldPoint(mouse2);
+                dir1.y = rig.position.y;
+                dir2.y = rig.position.y;
+               //屏幕鼠标确定小球方向
+                dir = dir2 - dir1;
+        
+                dir = dir.normalized; 
+                tspeed = trspeed=0;//为了测试把速度变为0,本来应该是用子弹时间做
+                Debug.DrawRay(rig.position, dir, Color.red);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //松开鼠标赋予速度,取消指示线的显示
+            show_line = false;
+            trspeed = rspeed;
+            tspeed = speed;
+        }
+        //小球滚动前进
+        Vector3 rotate_dir = Vector3.Cross(Vector3.up,dir);
+        transform.Rotate(rotate_dir * trspeed,Space.World);
+    }
+    private void FixedUpdate()
+    {
+        rig.velocity = dir * tspeed;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        //计算完全弹性碰撞更新移动方向
+        if(collision.gameObject.tag=="wall")
+        {
+           ContactPoint cp = collision.contacts[0];
+            Vector3 rdir = Vector3.Reflect(dir, cp.normal);
+            rdir.y = 0;
+            dir = rdir.normalized;
+        }
+    }
+}
---
+++ NEMiniGame/Assets/Scripts/line.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class line : MonoBehaviour
+{
+    public GameObject sphere;
+    public PlayerControl Player;
+    public LineRenderer linerender;
+    public float line_length;
+    public Vector3 p0, p1, p2;
+    private Vector3 curdir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = GameObject.FindGameObjectWithTag("Player");
+        if (sphere != null)
+            Player = sphere.GetComponent<PlayerControl>();
+        linerender = GetComponent<LineRenderer>();
+        linerender.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Player.show_line) linerender.enabled = false;
+        else linerender.enabled = true;
+
+        p0 = sphere.transform.position;
+        p0.y -= (float)0.5;
+        curdir = Player.dir;
+        //Debug.Log(curdir);
+        linerender.SetPosition(0, p0);
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(p0, curdir, out hit, line_length, 1 << LayerMask.NameToLayer("wall")))
+        {
+
+            linerender.positionCount = 3;
+            Vector3 rdir = Vector3.Reflect(curdir, hit.normal);
+            rdir.y = 0;
+            float uselength = Vector3.Distance(hit.point, p0);
+            p1 = p0 + uselength * curdir;
+            p2 = hit.point + rdir * (line_length - uselength);
+            linerender.SetPosition(1, p1);
+            linerender.SetPosition(2, p2);
+        }
+        else
+        {
+            linerender.positionCount = 2;
+            p1 = p0 + line_length * curdir;
+            linerender.SetPosition(1, p1);
+
+        }
+
+
+    }
+}
