29f353cd4434edac5ba34bbcc5413dcfea538355
ShaYaZhou <sy_Zhou@pku.edu.cn>
2019-05-31 20:46:25 +0800

    修复相机，添加模型，调整AI

---
+++ NEMiniGame/Assets/Scripts/CamController.cs
@@ -0,0 +1,35 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public GameObject[] floors;
+    public Transform target;
+    public float[] xSizes;
+    private float size=0;
+    CinemachineTrackedDolly ctd;
+    private float num;
+    private void Awake()
+    {
+        xSizes = new float[floors.Length];
+        for (int i = 0; i < floors.Length; i++)
+        {
+            xSizes[i] = floors[i].GetComponent<MeshFilter>().mesh.bounds.size.x * floors[i].transform.localScale.x;
+            size += floors[i].GetComponent<MeshFilter>().mesh.bounds.size.x * floors[i].transform.localScale.x;
+        }
+        num = floors.Length;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ctd = gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ctd.m_PathPosition = target.position.x * num / size;
+    }
+}
