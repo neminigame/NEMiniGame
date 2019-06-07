e551c09c3cb92b55ecb9a348f2b42d9035fa83c6
mxrhyx233 <mxr233456@sina.com>
2019-06-08 01:34:51 +0800

    fix

--- NEMiniGame/Assets/Scripts/zhedangbantou.cs
+++ NEMiniGame/Assets/Scripts/zhedangbantou.cs
@@ -12,7 +12,7 @@ public class zhedangbantou : MonoBehaviour {
         public bool flag = false;
       
     }
-    public Transform targetobject = null;
+    public Transform[] targetobject = null;
     public float TranspartentPower = 0.2f;
     public float fadespeed = 1f;
     private Dictionary<Renderer, mxr> hyx = new Dictionary<Renderer, mxr>();
@@ -25,7 +25,7 @@ public class zhedangbantou : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (targetobject == null) return;
+        if (targetobject.Length<1) return;
         SetTransparent();
         UpdateTransparent();
         ClearTransparent();
@@ -40,7 +40,7 @@ public class zhedangbantou : MonoBehaviour {
             foreach(var t in temp.materials)
             {
                 Color c = t.GetColor("_Color");
-                temp.ctime += Time.deltaTime;
+                temp.ctime += Time.unscaledDeltaTime;
                 c.a = Mathf.Lerp(1, TranspartentPower, temp.ctime*fadespeed);
                 t.SetColor("_Color", c);
             }
@@ -49,32 +49,35 @@ public class zhedangbantou : MonoBehaviour {
     public void UpdateTransparent()
     {
         RaycastHit[] hit;
-        Vector3 viewdir = (targetobject.position - transform.position).normalized;
-        float dis = Vector3.Distance(transform.position, targetobject.position);
-        hit = Physics.RaycastAll(new Ray(transform.position, viewdir),dis, judgelayer);
-        Debug.DrawLine(transform.position, targetobject.position);
-        foreach(var t in hit)
+        for(int i=0;i< targetobject.Length;i++)
         {
-            Renderer[] render = t.transform.GetComponentsInChildren<Renderer>();
-            foreach(var it in render)
+            Vector3 viewdir = (targetobject[i].position - transform.position).normalized;
+            float dis = Vector3.Distance(transform.position, targetobject[i].position);
+            hit = Physics.RaycastAll(new Ray(transform.position, viewdir), dis, judgelayer);
+            foreach (var t in hit)
             {
-                mxr parm = null;
-                hyx.TryGetValue(it, out parm);
-                if(parm==null)
+                Renderer[] render = t.transform.GetComponentsInChildren<Renderer>();
+                foreach (var it in render)
                 {
-                    parm = new mxr();
-                    //如果先调用materials,后调用的shadermaterial是调用materials就会生成的那个。
-                    //https://blog.csdn.net/xxxwuyufan/article/details/82181248
-                    parm.shadermaterials = it.sharedMaterials;
-                    parm.materials = it.materials;
-                     hyx.Add(it, parm);
-                    foreach(var c in parm.materials)
-                        c.shader = Shader.Find("MiniGame/toon_alpha");
+                    mxr parm = null;
+                    hyx.TryGetValue(it, out parm);
+                    if (parm == null)
+                    {
+                        parm = new mxr();
+                        //如果先调用materials,后调用的shadermaterial是调用materials就会生成的那个。
+                        //https://blog.csdn.net/xxxwuyufan/article/details/82181248
+                        parm.shadermaterials = it.sharedMaterials;
+                        parm.materials = it.materials;
+                        hyx.Add(it, parm);
+                        foreach (var c in parm.materials)
+                            c.shader = Shader.Find("MiniGame/toon_alpha");
+                    }
+
+                    parm.flag = true;
                 }
-            
-                parm.flag = true;
             }
         }
+ 
     }
     public void ClearTransparent()
     {
--- NEMiniGame/Assets/Shader/toon_alpha.shader
+++ NEMiniGame/Assets/Shader/toon_alpha.shader
@@ -12,20 +12,17 @@ Shader "MiniGame/toon_alpha"
 		_shadowColor("shadow color",Color)=(0.1,0.1,0.1,0.1)
 	}
 	SubShader{
-
+		Tags{ "Queue" = "Transparent" "RenderType" = "Transparent" "IgnoreProjector" = "True"}
 		Pass
 		{
-			Tags{"LightMode"="ForwardBase"  "Queue" = "Transparent"}
+			
 			ZWrite Off
 			Blend SrcAlpha OneMinusSrcAlpha
 			CGPROGRAM
 
 			#pragma vertex vert
 			#pragma fragment frag
-			#include "Lighting.cginc"
 			#include "UnityCG.cginc"
-			#include "AutoLight.cginc"
-			#pragma multi_compile_fwdbase
 			sampler2D _MainTex;
 			float4 _MainTex_ST;
 			fixed4 _Color,_shadowColor;
@@ -35,7 +32,6 @@ Shader "MiniGame/toon_alpha"
 				float3 worldnormal:TEXCOORD0;
 				float3 worldpos:TEXCOORD1;
 				half2 uv:TEXCOORD2;
-				SHADOW_COORDS(3)
 			};
 			v2f vert(appdata_base v)
 			{
@@ -44,23 +40,15 @@ Shader "MiniGame/toon_alpha"
 				o.worldpos=mul(unity_ObjectToWorld,v.vertex).xyz;
 				o.worldnormal=UnityObjectToWorldNormal(v.normal);
 				o.uv=TRANSFORM_TEX(v.texcoord,_MainTex);
-				TRANSFER_SHADOW(o);
 				return o;
 			}
 			fixed4 frag(v2f i):SV_Target
 			{
 				fixed3 worldnormal = normalize(i.worldnormal);
-				fixed3 worldlightdir=normalize(UnityWorldSpaceLightDir(i.worldpos));
-				fixed3 worldviewdir=normalize(UnityWorldSpaceViewDir(i.worldpos));
-				fixed3 halfdir=normalize(worldlightdir+worldviewdir);
 				fixed3 albedo=_Color.rgb;
-				UNITY_LIGHT_ATTENUATION(atten,i,i.worldpos);
-				fixed3 ambient=UNITY_LIGHTMODEL_AMBIENT.xyz*albedo;
-				fixed diff=dot(worldnormal,worldlightdir)*0.5+0.5;
-				fixed3 diffuse=_LightColor0.rgb*albedo;
+				fixed3 diffuse=albedo;
 
-				diffuse=lerp(_shadowColor*diffuse,diffuse,atten);
-				return fixed4((ambient+diffuse)*_light,_Color.a);
+				return fixed4((diffuse),_Color.a);
 			}
 			ENDCG
 		}
