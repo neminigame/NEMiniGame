c079e105ba4e53c05a10ad23b692b79dd4e82cd3
ShaYaZhou <sy_Zhou@pku.edu.cn>
2019-06-10 16:27:40 +0800

    转场scanningLine，添加模型，解决阴影锯齿

---
+++ NEMiniGame/Assets/Scripts/ScanningLine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanningLine : MonoBehaviour
+{
+    public Material TransitionMat;
+    public bool canPlay=false;
+    private float _time;
+    public float speed = 1;
+    public bool distort;
+    private void Awake()
+    {
+        Initiate();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canPlay)
+        {
+            _time += Time.deltaTime;
+            TransitionMat.SetFloat("_Cutoff", speed *_time);
+            if (speed * _time > 1)
+            {
+                canPlay = false;
+                _time = 0;
+            }
+        }
+        TransitionMat.SetFloat("_Distort", Convert.ToInt32(distort));
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, TransitionMat);
+    }
+    private void Initiate()
+    {
+        if (!TransitionMat)
+        {
+            Shader shader = Shader.Find("JackieZhou/Distort");
+            TransitionMat = new Material(shader);
+        }
+        TransitionMat.SetFloat("_Cutoff", 0);
+        canPlay = false;
+        _time = 0;
+    }
+}
